60e88b17e51088c3c8fdba64b996bc68b1caa5a0
Kairi <kairi.20081001@gmail.com>
2023-12-27 10:17:26 +0900

    init

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZJyYFxpeLnk0NDwLgdgviELIbofOu_ZE",
+  authDomain: "front-quiz-3cd1d.firebaseapp.com",
+  projectId: "front-quiz-3cd1d",
+  storageBucket: "front-quiz-3cd1d.appspot.com",
+  messagingSenderId: "731043815607",
+  appId: "1:731043815607:web:7fd0e1c23e73d61761ed32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
